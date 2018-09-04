4e9cd349ce425e17f38925aa3c19285170cd1aee
Jordan Simon <jordan.simon55@gmail.com>
2018-09-04 18:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function dwarfRollCall(dwarves) {
+  var dwarf = []
+  for (var i = 0; i < dwarves.length; i++) {
+    dwarf.push(`${i + 1}. ${dwarves[i]}`)
+  } return dwarf.join('')
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+  var planeteerLoudCalls = []
+  for (var i = 0; i <planeteerCalls.length; i++) {
+    planeteerLoudCalls.push(`${planeteerCalls[i].toUpperCase()}!`)
+  } return planeteerLoudCalls
 }
 
 function longPlaneteerCalls(words) {
